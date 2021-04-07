e1946ba78037f18c5fc8c37fc59767799f3614fa
Diego Lima <dimaslsoares@gmail.com>
2021-04-07 15:44:40 -0300

    Exercício de fixação sobre exceções criado

---
+++ src/model/exceptions/ExcepctionAccount.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class ExcepctionAccount extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ExcepctionAccount(String msg) {
+		super(msg);
+	}
+	
+}
